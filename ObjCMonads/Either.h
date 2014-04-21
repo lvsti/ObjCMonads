e447ef3b82debae1e6e85c9ce5daa5a414146f82
Lk Xf <elveestei@gmail.com>
2014-04-21 14:52:34 +0200

    Rewrote Either with type cluster

--- ObjCMonads/Either.h
+++ ObjCMonads/Either.h
@@ -9,11 +9,15 @@
 #import <Foundation/Foundation.h>
 #import "Functor.h"
 #import "Monad.h"
+#import "TypeCluster.h"
 
 @class List;
 @class Tuple;
 
-@interface Either : NSObject<Monad, Functor, NSCopying>
+@interface Either : TypeCluster<Monad, Functor, NSCopying>
+
++ (Either*)left:(id)lvalue;
++ (Either*)right:(id)rvalue;
 
 @end
 
@@ -23,7 +27,21 @@ extern "C" {
 
     Either* Left(id lvalue);
     Either* Right(id rvalue);
+    
+    // either :: (a -> c) -> (b -> c) -> Either a b -> c
+    id WithEither(id(^funcAC)(id), id(^funcBC)(id), Either OF(a, b)* either);
+
+    // lefts :: [Either a b] -> [a]
+    List* Lefts(List* eithers);
+
+    // rights :: [Either a b] -> [b]
+    List* Rights(List* eithers);
+    
+    // partitionEithers :: [Either a b] -> ([a],[b])
+    Tuple* PartitionEithers(List* eithers);
+    
     BOOL IsLeft(Either* e);
+    BOOL IsRight(Either* e);
     id FromLeft(Either* e);
     id FromRight(Either* e);
 
--- ObjCMonads/Either.m
+++ ObjCMonads/Either.m
@@ -12,8 +12,7 @@
 
 
 @interface Either ()
-@property (nonatomic, strong) id left;
-@property (nonatomic, strong) id right;
+@property (nonatomic, strong) id value;
 @property (nonatomic, assign) BOOL isLeft;
 
 - (instancetype)initWithLeft:(id)lvalue;
@@ -23,41 +22,92 @@
 
 
 Either* Left(id lvalue) {
-    return [[Either alloc] initWithLeft:lvalue];
+    return [Either left:lvalue];
 }
 
 Either* Right(id rvalue) {
-    return [[Either alloc] initWithRight:rvalue];
+    return [Either right:rvalue];
+}
+
+id WithEither(id(^funcAC)(id), id(^funcBC)(id), Either OF(a, b)* either) {
+    assert(either);
+    if (either.isLeft) {
+        assert(funcAC);
+        return funcAC(either.value);
+    }
+    
+    assert(funcBC);
+    return funcBC(either.value);
+}
+
+List* Lefts(List* eithers) {
+    assert(eithers);
+    return FoldR(^id(Either* obj, List* accum) {
+        return obj.isLeft? Cons(obj, accum): accum;
+    }, Empty(), eithers);
+}
+
+List* Rights(List* eithers) {
+    assert(eithers);
+    return FoldR(^id(Either* obj, List* accum) {
+        return !obj.isLeft? Cons(obj, accum): accum;
+    }, Empty(), eithers);
+}
+
+Tuple* PartitionEithers(List* eithers) {
+    assert(eithers);
+    return FoldR(^id(Either* obj, Tuple* accum) {
+        return obj.isLeft? MkPair(Cons(obj, accum[0]), accum[1]): MkPair(accum[0], Cons(obj, accum[1]));
+    }, MkPair(Empty(), Empty()), eithers);
 }
 
 BOOL IsLeft(Either* e) {
     return e.isLeft;
 }
 
+BOOL IsRight(Either* e) {
+    return !e.isLeft;
+}
+
 id FromLeft(Either* e) {
-    return e.left;
+    assert(e.isLeft);
+    return e.value;
 }
 
 id FromRight(Either* e) {
-    return e.right;
+    assert(!e.isLeft);
+    return e.value;
 }
 
 
 @implementation Either
 
++ (Either*)left:(id)lvalue {
+    return [[Either alloc] initWithLeft:lvalue];
+}
+
++ (Either*)right:(id)rvalue {
+    return [[Either alloc] initWithRight:rvalue];
+}
+
+
 - (instancetype)initWithLeft:(id)lvalue {
-    self = [super init];
+    assert(lvalue);
+    self = [super initWithClusterClass:[Either class]
+                            parameters:@[lvalue, TCFreeParameter()]];
     if (self) {
-        _left = lvalue;
+        _value = lvalue;
         _isLeft = YES;
     }
     return self;
 }
 
 - (instancetype)initWithRight:(id)rvalue {
-    self = [super init];
+    assert(rvalue);
+    self = [super initWithClusterClass:[Either class]
+                            parameters:@[TCFreeParameter(), rvalue]];
     if (self) {
-        _right = rvalue;
+        _value = rvalue;
         _isLeft = NO;
     }
     return self;
@@ -67,25 +117,37 @@ id FromRight(Either* e) {
     return self;
 }
 
-- (id)left {
-    assert(_isLeft);
-    return _left;
-}
-
-- (id)right {
-    assert(!_isLeft);
-    return _right;
+- (BOOL)isEqual:(id)other {
+    if (self == other) {
+        return YES;
+    }
+    
+    if (!other) {
+        return NO;
+    }
+    
+    if (![[self class] isCompatibleWithClass:[other class]]) {
+        return NO;
+    }
+    
+    return _isLeft == ((Either*)other).isLeft && [_value isEqual:((Either*)other).value];
 }
 
 - (NSString*)description {
-    return _isLeft? [NSString stringWithFormat:@"Left %@", _left]: [NSString stringWithFormat:@"Right %@", _right];
+    return [NSString stringWithFormat:@"%@ %@", (_isLeft? @"Left": @"Right"), _value];
 }
 
 #pragma mark - Functor:
 
 + (id<Functor>(^)(Mapping, id<Functor>))fmap {
-    return ^Either*(Mapping map, Either* ftor) {
-        return ftor.isLeft? [ftor copy]: Right(map(ftor.right));
+    return ^Either*(Mapping func, Either* ftor) {
+        assert(ftor);
+        if (!ftor.isLeft) {
+            assert(func);
+            return [Either right:func(ftor.value)];
+        }
+        
+        return [ftor copy];
     };
 }
 
@@ -93,18 +155,19 @@ id FromRight(Either* e) {
 
 + (MonadicValue(^)(MonadicValue, Continuation))bind {
     return ^MonadicValue(Either* mvalue, Continuation cont) {
+        assert(mvalue);
         if (!mvalue.isLeft) {
+            assert(cont);
             return cont(mvalue.value, self);
         }
         
-        // nothing
         return [mvalue copy];
     };
 }
 
 + (MonadicValue(^)(id))unit {
     return ^Either*(id value) {
-        return Right(value);
+        return [Either right:value];
     };
 }
 
